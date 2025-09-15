e134f1858f3fc382337166fbc74568b36e981369
Mahedi Hasan <mahedi.hasan@byslglobal.com>
2025-09-15 18:05:28 +0600

    Report Generation process Updated

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,13 +1,25 @@
 package runners;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
-	    features = "src/test/resources/features",
-	    glue = "stepsDefs",
-	    plugin = {"pretty", "html:target/cucumber-report.html"},
-	    monochrome = true
-	)
-public class TestRunner extends AbstractTestNGCucumberTests{
+		features = "src/test/resources/features",
+		glue = {"stepsDefs"},
+		plugin = {
+				"pretty",
+				"html:target/cucumber-reports/cucumber.html",
+				"json:target/cucumber-reports/cucumber.json",
+				"junit:target/cucumber-reports/cucumber.xml"
+		},
+		monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
 
+	@Test
+	public void runCukes() {
+		// This is just a placeholder for Gradle to detect a test
+		// Actual execution is handled by AbstractTestNGCucumberTests
+	}
 }
